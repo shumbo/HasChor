5e256699cffcdba8357127c04d423420b8b0ab9f
Shun Kashiwa <shunthedev@gmail.com>
2023-02-19 22:33:31 -0800

    migrate `mergesort` to new API

--- app/mergesort/Main.hs
+++ app/mergesort/Main.hs
@@ -36,13 +36,13 @@ sort ::
   ([Int] @ a) ->
   Choreo IO ([Int] @ a)
 sort a b c lst = do
-  condition <- a `locallyDo` \unwrap -> do return $ length (unwrap lst) > 1
-  cond a condition \case
+  condition <- a `locally` \unwrap -> do return $ length (unwrap lst) > 1
+  cond (a, condition) \case
     True -> do
-      pivot <- a `locallyDo` \unwrap -> do return $ length (unwrap lst) `div` 2
-      divided <- a `locallyDo` \unwrap -> do return $ divide (unwrap lst)
-      l <- a `locallyDo` \unwrap -> do return $ fst (unwrap divided)
-      r <- a `locallyDo` \unwrap -> do return $ snd (unwrap divided)
+      pivot <- a `locally` \unwrap -> do return $ length (unwrap lst) `div` 2
+      divided <- a `locally` \unwrap -> do return $ divide (unwrap lst)
+      l <- a `locally` \unwrap -> do return $ fst (unwrap divided)
+      r <- a `locally` \unwrap -> do return $ snd (unwrap divided)
       l' <- (a, l) ~> b
       r' <- (a, r) ~> c
       ls' <- sort b c a l'
@@ -62,30 +62,30 @@ merge ::
   [Int] @ c ->
   Choreo IO ([Int] @ a)
 merge a b c lhs rhs = do
-  lhsHasElements <- b `locallyDo` \unwrap -> do return $ not (null (unwrap lhs))
-  cond b lhsHasElements \case
+  lhsHasElements <- b `locally` \unwrap -> do return $ not (null (unwrap lhs))
+  cond (b, lhsHasElements) \case
     True -> do
-      rhsHasElements <- c `locallyDo` \unwrap -> do return $ not (null (unwrap rhs))
-      cond c rhsHasElements \case
+      rhsHasElements <- c `locally` \unwrap -> do return $ not (null (unwrap rhs))
+      cond (c, rhsHasElements) \case
         True -> do
-          rhsHeadAtC <- c `locallyDo` \unwrap -> do return $ head (unwrap rhs)
+          rhsHeadAtC <- c `locally` \unwrap -> do return $ head (unwrap rhs)
           rhsHeadAtB <- (c, rhsHeadAtC) ~> b
-          takeLhs <- b `locallyDo` \unwrap -> do return $ head (unwrap lhs) <= unwrap rhsHeadAtB
-          cond b takeLhs \case
+          takeLhs <- b `locally` \unwrap -> do return $ head (unwrap lhs) <= unwrap rhsHeadAtB
+          cond (b, takeLhs) \case
             True -> do
               -- take (head lhs) and merge the rest
-              lhs' <- b `locallyDo` \unwrap -> do return $ tail (unwrap lhs)
+              lhs' <- b `locally` \unwrap -> do return $ tail (unwrap lhs)
               merged <- merge a b c lhs' rhs
-              lhsHeadAtB <- b `locallyDo` \unwrap -> do return $ head (unwrap lhs)
+              lhsHeadAtB <- b `locally` \unwrap -> do return $ head (unwrap lhs)
               lhsHeadAtA <- (b, lhsHeadAtB) ~> a
-              a `locallyDo` \unwrap -> do return $ unwrap lhsHeadAtA : unwrap merged
+              a `locally` \unwrap -> do return $ unwrap lhsHeadAtA : unwrap merged
             False -> do
               -- take (head rhs) and merge the rest
-              rhs' <- c `locallyDo` \unwrap -> do return $ tail (unwrap rhs)
+              rhs' <- c `locally` \unwrap -> do return $ tail (unwrap rhs)
               merged <- merge a b c lhs rhs'
-              rhsHeadAtC <- c `locallyDo` \unwrap -> do return $ head (unwrap rhs)
+              rhsHeadAtC <- c `locally` \unwrap -> do return $ head (unwrap rhs)
               rhsHeadAtA <- (c, rhsHeadAtC) ~> a
-              a `locallyDo` \unwrap -> do return $ unwrap rhsHeadAtA : unwrap merged
+              a `locally` \unwrap -> do return $ unwrap rhsHeadAtA : unwrap merged
         False -> do
           (b, lhs) ~> a
     False -> do
@@ -93,9 +93,9 @@ merge a b c lhs rhs = do
 
 mainChoreo :: Choreo IO ()
 mainChoreo = do
-  lst <- primary `locallyDo` \unwrap -> do return [1, 6, 5, 3, 4, 2, 7, 8]
+  lst <- primary `locally` \unwrap -> do return [1, 6, 5, 3, 4, 2, 7, 8]
   sorted <- sort primary worker1 worker2 lst
-  primary `locallyDo` \unwrap -> do
+  primary `locally` \unwrap -> do
     print (unwrap sorted)
     return ()
   return ()
